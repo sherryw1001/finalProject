acc9f8254d2787d38129d99d517c1f0bb44d23d1
Sherry Wang <sherry.wang.1001@gmail.com>
2024-07-26 12:11:53 -0500

    Initial Commit

---
+++ finalPproject/finalPprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalPprojectApp.swift
+//  finalPproject
+//
+//  Created by Sherry Wang on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct finalPprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
