357d251933e4d876c08531d5cad78dcc18258bbe
summer <boongirl13@gmail.com>
2024-07-31 17:14:57 -0400

    changed a bunch of stuuf and added pages

---
+++ whatIsDeafCulture.swift
@@ -0,0 +1,18 @@
+//
+//  whatIsDeafCulture.swift
+//  finalPproject
+//
+//  Created by Summer on 7/31/24.
+//
+
+import SwiftUI
+
+struct whatIsDeafCulture: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    whatIsDeafCulture()
+}
